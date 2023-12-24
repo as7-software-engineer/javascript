c3acc0eb593c818c86f9f874fb62a8e461bc45c4
Santos Santos <154750702+as7-software-engineer@users.noreply.github.com>
2023-12-24 16:17:42 -0300

    Initial commit

---
+++ cev/a2cfc/a2.js
@@ -0,0 +1,5 @@
+"use strict" //modo estrito
+let nome="bruno"
+console.log(nome)
+console.log("bruno")
+console.log("nome: " + nome)
---
+++ cev/a3cfc/a3.js
@@ -0,0 +1,7 @@
+"use strict"
+let nome= "bruno"
+nome="cfb cursos"
+nome=10
+const curso="javascript"
+curso="html"//xxx erro
+console.log(curso)
